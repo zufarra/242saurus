7a97cbf5005fee8adab228925583bc6f8b6ec1d3
zufarra <zufarromli@gmail.com>
2024-10-29 11:56:46 +0700

    Deploy website - based on 17adfdb0374f79117a89bb468ca7b093ef5cf519

---
+++ assets/js/0a5f9bfb.11f8da40.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus=self.webpackChunkdocusaurus||[]).push([[3626],{3283:s=>{s.exports=JSON.parse('{"metadata":{"permalink":"/242saurus/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.f9585bea.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocusaurus=self.webpackChunkdocusaurus||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/242saurus/blog","blogTitle":"Blog","authorsListPath":"/242saurus/blog/authors"}')}}]);
